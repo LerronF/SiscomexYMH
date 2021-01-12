f68eef5e1d40d6f2e3f9c52cf7c06ad06a9a9843
lerronf <lerron@outlook.com>
2021-01-12 19:39:32 -0400

    Commit inicial

---
+++ Siscomex.Shared/LogController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Siscomex.Shared
+{
+    public static class LogController
+    {
+        public static void RegistrarLog(string msg, eTipoLog tipo = eTipoLog.INFO)
+        {
+            EscreverLog(msg, tipo.GetDescription());
+        }
+
+        private static void EscreverLog(string msg, string tipo)
+        {
+            Console.WriteLine($"{tipo} [ {DateTime.Now} ]: {msg}");
+        }
+    }
+}
---
+++ Siscomex/Service1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace Siscomex
+{
+    public partial class Service1 : ServiceBase
+    {
+        private Timer _timer;
+
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            _timer = new Timer(CriarLog, null, 0, 120000);
+        }
+
+        protected override void OnStop()
+        {
+        }
+
+        private void CriarLog(object state)
+        {
+
+
+            StreamWriter vWriter = new StreamWriter(@"C:\Logs\log.txt", true);
+            vWriter.WriteLine("serviço rodando:" + DateTime.Now);
+            vWriter.Flush();
+            vWriter.Close();
+        }
+    }
+}
